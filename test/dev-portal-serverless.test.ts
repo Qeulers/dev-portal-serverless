69870aeba75debed53aae324189ca3d98597f201
Qeulers <frank.keulers@protonmail.com>
2024-12-20 10:41:46 +0000

    Initial commit

---
+++ test/dev-portal-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DevPortalServerless from '../lib/dev-portal-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dev-portal-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DevPortalServerless.DevPortalServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
